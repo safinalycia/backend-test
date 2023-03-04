f9810667bbdb04723827f55f91de7ca68554269b
lycia <safinalycia@mail.ru>
2023-03-04 21:59:56 +0300

    загружено 4 дз

---
+++ backend_test/src/main/java/service/Endpoints.java
@@ -0,0 +1,9 @@
+package service;
+public final class Endpoints {
+
+    public static final String getSearchRecipes = "recipes/complexSearch";
+
+    public static final String postClassifyCuisine = "recipes/cuisine";
+
+    public static final String mealPlan = "https://api.spoonacular.com/mealplanner/171819rai/items/";
+}
